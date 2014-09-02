8c001952e78f520f7c25f53f065b5c861d899e70
Daniel Eden <dte@dropbox.com>
2014-09-01 20:26:18 -0700

    Updae makefile

--- make.sh
+++ make.sh
@@ -13,19 +13,19 @@ files="gitconfig gitignore zshrc oh-my-zsh scss-lint.yml"    # list of files/fol
 ##########
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
+printf "Creating $olddir for backup of any existing dotfiles in ~\n"
 mkdir -p $olddir
-echo "...done"
+printf "...done\n\n"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
+printf "Changing to the $dir directory\n"
 cd $dir
-echo "...done"
+printf "...done\n\n"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
+    printf "Moving any existing dotfiles from ~ to $olddir\n"
+    mv ~/.$file $olddir
+    printf "Creating symlink to $file in home directory.\n\n"
     ln -s $dir/$file ~/.$file
 done
