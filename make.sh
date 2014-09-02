be0d5b6d91d53759034ab97600116b74a50a895e
Daniel Eden <dte@dropbox.com>
2014-09-02 13:23:15 -0700

    Greatly improve speed of cd
    Disabling untracked files as dirty means the git status check on cd is much faster

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ############################
 # .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# This script creates symlinks from the home directory to any desired dotfiles in ~/.dotfiles
 ############################
 
 ########## Variables
