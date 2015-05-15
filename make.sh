6ec0eba1c121b4c26cdcc61b4adb9a641be840b9
Daniel Eden <dan.eden@me.com>
2015-05-14 20:08:23 -0700

    Add pip installation for Linux

--- make.sh
+++ make.sh
@@ -12,7 +12,11 @@ olddir=~/.dotfiles_old/$now      # old dotfiles backup directory
 files="gitconfig gitignore zshrc oh-my-zsh scss-lint.yml zfuncs vimrc vim zprivate"    # list of files/folders to symlink in homedir
 nodot="Brewfile" # list of non-dot files to symlink
 
-##########
+#------------------------------
+#
+# Housekeeping
+#
+#------------------------------
 
 # create dotfiles_old in homedir
 printf "Creating $olddir for backup of any existing dotfiles in ~\n"
@@ -40,16 +44,29 @@ for file in $nodot; do
 	chflags -h hidden ~/$file
 done
 
+#------------------------------
+#
+# Package manager stuff
+#
+#------------------------------
+
+# Install pip and Codemod
+echo "Installing pip and Codemod.\n"
+echo "We're going to need your password.\n"
+
 if [ "$(uname)" == "Darwin" ]; then
-  echo "Installing pip and Codemod.\n"
-  echo "We're going to need your password.\n"
   sudo easy_install pip
-  sudo pip install git+https://github.com/facebook/codemod.git
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  sudo apt-get install pip
 fi
+
+sudo pip install git+https://github.com/facebook/codemod.git
+
 
 # Check presence of Homebrew, add bundler, then install our desired recipes & update them
 type brew &>/dev/null && printf "`brew tap Homebrew/bundle && brew bundle &&
   brew update`" || echo "`ruby -e \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\" && brew tap Homebrew/bundle && brew bundle && brew update`"
+
 
 # Install Vim plugins
 vim +PluginInstall +qall
