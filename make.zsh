7b00653e7f89c49990832f3b2a73e1f64d7834f1
Daniel Eden <dan.eden@me.com>
2015-05-23 13:39:33 -0700

    Switch to Prezto from oh-my-zsh
    It's faster, and git completion actually works

--- make.zsh
+++ make.zsh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 ############################
 # .make.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/.dotfiles
@@ -6,10 +6,10 @@
 
 ########## Variables
 
-dir=~/.dotfiles                    # dotfiles directory
+dir=$HOME/.dotfiles                    # dotfiles directory
 now=`date +%Y-%m-%d-%H:%M:%S`
-olddir=~/.dotfiles_old/$now      # old dotfiles backup directory
-files="gitconfig gitignore zshrc oh-my-zsh scss-lint.yml zfuncs vimrc vim zprivate"    # list of files/folders to symlink in homedir
+olddir=$HOME/.dotfiles_old/$now      # old dotfiles backup directory
+files="gitconfig gitignore zfuncs vimrc vim zprivate"    # list of files/folders to symlink in homedir
 nodot="Brewfile" # list of non-dot files to symlink
 
 #------------------------------
@@ -31,18 +31,31 @@ printf "...done\n\n"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 printf "Moving any existing dotfiles from ~ to $olddir\n"
 for file in $files; do
-  mv ~/.$file $olddir
+  mv $HOME/.$file $olddir
   printf "Creating symlink to $file in home directory.\n"
-  ln -s $dir/$file ~/.$file
+  ln -s $dir/$file $HOME/.$file
 done
 
 printf "\nMoving no-dot files from ~ to $olddir\n"
 for file in $nodot; do
-	mv ~/$file $olddir
+	mv $HOME/$file $olddir
 	printf "Creating symlink to $file in home directory\n\n"
-	ln -s $dir/$file ~/$file
-	chflags -h hidden ~/$file
+	ln -s $dir/$file $HOME/$file
+	chflags -h hidden $HOME/$file
 done
+
+if [ ! -d "$HOME/.zprezto" ]; then
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+  setopt EXTENDED_GLOB
+  for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  done
+else
+  cd $HOME/.zprezto && git pull && git submodule update --init --recursive
+fi
+
+cd $dir && ln -sf $dir/zpreztorc $HOME/.zpreztorc
 
 #------------------------------
 #
@@ -54,7 +67,7 @@ done
 echo "Installing pip and Codemod."
 echo "We're going to need your password."
 
-if [ "$(uname)" == "Darwin" ]; then
+if [[ `uname` == "Darwin" ]]; then
   sudo easy_install pip
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   sudo apt-get install pip
