002ca0faa97308df40d14b5ff662106d0c91c7b0
Daniel Eden <dan.eden@me.com>
2015-05-23 21:38:47 -0700

    Remove unused stuff

--- make.zsh
+++ make.zsh
@@ -1,4 +1,27 @@
 #!/bin/zsh
+
+# First, let's get some helpful colors.
+red=$'\e[1;31m'
+yel=$'\e[0;35m'
+grn=$'\e[0;32m'
+end=$'\e[0m'
+
+msg() {
+  printf "${yel}$*${end}\n"
+}
+
+suc() {
+  printf "${grn}$*${end}\n"
+}
+
+warn() {
+  printf "${red}$*${end}\n"
+}
+
+don() {
+  printf "...done\n\n"
+}
+
 ############################
 # .make.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/.dotfiles
@@ -9,7 +32,7 @@
 dir=$HOME/.dotfiles                    # dotfiles directory
 now=`date +%Y-%m-%d-%H:%M:%S`
 olddir=$HOME/.dotfiles_old/$now      # old dotfiles backup directory
-files=("gitconfig" "gitignore" "zfuncs" "vimrc" "vim" "zprivate")    # list of files/folders to symlink in homedir
+files=("gitconfig" "gitignore" "vimrc" "vim" "zprivate")    # list of files/folders to symlink in homedir
 nodot=("Brewfile") # list of non-dot files to symlink
 
 #------------------------------
@@ -19,45 +42,46 @@ nodot=("Brewfile") # list of non-dot files to symlink
 #------------------------------
 
 # create dotfiles_old in homedir
-printf "Creating $olddir for backup of any existing dotfiles in ~\n"
+msg "Creating $olddir for backup of any existing dotfiles in $HOME..."
 mkdir -p $olddir
-printf "...done\n\n"
+don
 
 # change to the dotfiles directory
-printf "Changing to the $dir directory\n"
 cd $dir
-printf "...done\n\n"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
-printf "Moving any existing dotfiles from ~ to $olddir\n"
+msg "Moving existing dotfiles from $HOME to $olddir..."
 for file in $files; do
   mv $HOME/.$file $olddir
-  printf "Creating symlink to $file in home directory.\n"
   ln -s $dir/$file $HOME/.$file
 done
 
-printf "\nMoving no-dot files from ~ to $olddir\n"
 for file in $nodot; do
 	mv $HOME/$file $olddir
-	printf "Creating symlink to $file in home directory\n\n"
 	ln -s $dir/$file $HOME/$file
 	chflags -h hidden $HOME/$file
 done
+don
 
 # Install Prezto
 if [ ! -d "$HOME/.zprezto" ]; then
+  msg "Installing Prezto..."
   git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 else
+  msg "Updating Prezto..."
   cd $HOME/.zprezto && git pull && git submodule update --init --recursive
-
 fi
+don
 
+# Create symlinks to Prezto dotfiles
+msg "Symlinking Prezto dotfiles..."
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
 cd $dir && ln -sf $dir/zpreztorc $HOME/.zpreztorc
+don
 
 #------------------------------
 #
@@ -66,8 +90,8 @@ cd $dir && ln -sf $dir/zpreztorc $HOME/.zpreztorc
 #------------------------------
 
 # Install pip and Codemod
-echo "Installing pip and Codemod."
-echo "We're going to need your password."
+msg "Installing pip and Codemod..."
+printf "Admin password may be required.\n"
 
 if [[ `uname` == "Darwin" ]]; then
   sudo easy_install pip
@@ -75,17 +99,28 @@ elif [[ `uname` == "Linux" ]]; then
   sudo apt-get install pip
 fi
 
-sudo -H pip install git+https://github.com/facebook/codemod.git
-
+sudo -H pip install git+https://github.com/facebook/codemod.git --upgrade
+don
 
 # Check presence of Homebrew, add bundler, then install our desired recipes & update them
-type brew &>/dev/null && printf "`brew tap Homebrew/bundle && brew bundle &&
-  brew update`" || echo "`ruby -e \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\" && brew tap Homebrew/bundle && brew bundle && brew update`"
-
+if [[ `uname` == "Darwin" ]]; then
+  msg "Installing Homebrew packages..."
+  type brew &>/dev/null && printf "`brew upgrade --all && brew bundle &&
+    brew update`" || echo "`ruby -e \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\" && brew tap Homebrew/bundle && brew bundle && brew update`"
+  don
+fi
 
 # Install Vim plugins
-vim +PluginInstall +qall
+msg "Installing Vim plugins..."
+vim +PluginClean +PluginInstall +PluginUpdate +qall
+don
 
-printf "\nAll done!\nYou may want to run 'brew upgrade', too.\n"
+# Switch to zsh if we're not already there
+if [[ ! `echo $SHELL` == "/bin/zsh" ]]; then
+  msg "Switching to zsh..."
+  chsh -s /bin/zsh
+  don
+fi
 
-chsh -s /bin/zsh
+suc "All done!"
+suc "Don't forget to tip your barista.\n"
