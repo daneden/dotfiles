e6a08fb2431eb9bf87b5b2863f54cacac64b06c7
Daniel Eden <dan.eden@me.com>
2016-01-12 12:28:10 -0800

    Add installation of vim-plugs

--- make.zsh
+++ make.zsh
@@ -192,6 +192,7 @@ msg "Installing setting up vim/neovim..."
 mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
 ln -s ~/.vim $XDG_CONFIG_HOME/nvim
 ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugClean! +PlugInstall! +qall
 don
 
