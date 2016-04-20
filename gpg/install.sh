fbc993c0e0734ee543b2a689fa28bd56920f3d78
daneden <dan.eden@me.com>
2016-04-20 09:13:57 -0700

    Update gpg options and installation scripts

--- gpg/install.sh
+++ gpg/install.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
-echo "# Enables GPG to find gpg-agent
-use-standard-socket
-
-" > $HOME/.gnupg/gpg-agent.conf
-
 if [ "$(uname)" == "Darwin" ]; then
   echo "# Connects gpg-agent to the OSX keychain via the brew-installed
 # pinentry program from GPGtools. This is the OSX 'magic sauce',
 # allowing the gpg key's passphrase to be stored in the login
 # keychain, enabling automatic key signing.
-pinentry-program /usr/local/bin/pinentry-mac" >> $HOME/.gnupg/gpg-agent.conf
+pinentry-program /usr/local/bin/pinentry-mac" > $HOME/.gnupg/gpg-agent.conf
 fi
 
 if [ tail -1 $HOME/.gnupg/gpg.conf != "use-agent" ]; then
