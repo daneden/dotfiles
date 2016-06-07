5d35bfeacac86532bbb3e4c7c1bad2ee048c4c0f
daneden <dan.eden@me.com>
2016-06-07 11:02:01 -0700

    Fix ruby install script

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-command -v rvm >/dev/null 2>&1 || { gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3; \curl -sSL https://get.rvm.io | bash -s stable --ruby; exit 1;}
-
-source "$HOME/.rvm/scripts/rvm"
+command -v rvm >/dev/null 2>&1 || { gpg --keyserver hkp://keys.gnupg.net --recv-keys \
+409B6B1796C275462A1703113804BB82D39DC0E3; \curl -sSL https://get.rvm.io | bash -s stable --ruby; \
+source "$HOME/.rvm/scripts/rvm"; exit 1;}
 
 rvm install ruby
 
