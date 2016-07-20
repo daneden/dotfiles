6d7cb85bb871d7ad4d19b22cf05db8566441e12b
daneden <dan.eden@me.com>
2016-07-20 16:15:59 -0700

    Update setup to adhere to shellcheck

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-username=`whoami`
+username=$(whoami)
 
 # Ask for the administrator password upfront
 sudo -v -p "\
   You're about to change a lot of things on your computer. We're gonna need a password.
   Enter password: "
 
-sudo ./script/bootstrap.sh -u $username
+sudo ./script/bootstrap.sh -u "$username"
