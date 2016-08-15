a1d7bd04bcc45d1fec2c16a206edcfb325a3d6e0
Daniel Eden <dan.eden@me.com>
2016-08-15 10:13:53 -0700

    Update to include Brewfile and NPM install scripts

---
+++ homebrew/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# Check if brew is installed, then run bundle to get latest packages listed in Brewfile
+
+cd "$(dirname $0)"/..
+
+if hash brew 2>/dev/null; then
+  brew tap Homebrew/bundle
+  brew bundle check || brew bundle
+fi
--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+# Install YADR
 sh -c "`curl -fsSL https://raw.githubusercontent.com/skwp/dotfiles/master/install.sh `"
 
+# Setup gitconfig, customizations, and extensions
 ./script/bootstrap.sh
+
+# Run package manager installation scripts
+./script/install
