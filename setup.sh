7aa38b068a0c6c6e5b0d4a5b4cb6729ba8df0788
Daniel Eden <dan.eden@me.com>
2016-08-15 09:44:14 -0700

    Update dotfiles to use YADR
    Turning my dotfiles repo into a customization extension directory for YADR.
    
    This just helps save my sanity and puts the burden of maintenance elsewhere.
    
    TODO: ensure the brewfile is still integrated, because homebrew is awesome.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
+sh -c "`curl -fsSL https://raw.githubusercontent.com/skwp/dotfiles/master/install.sh `"
 
-username=$(whoami)
-
-# Ask for the administrator password upfront
-sudo -v -p "\
-  You're about to change a lot of things on your computer. We're gonna need a password.
-  Enter password: "
-
-sudo ./script/bootstrap.sh -u "$username"
+./script/bootstrap.sh
