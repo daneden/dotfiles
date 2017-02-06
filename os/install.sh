fd2d62dd8b55eb23b0a37a6020d6e14d2d21024f
Daniel Eden <dan.eden@me.com>
2017-02-06 10:52:25 -0800

    Update hyper config and macOS tweaks

--- os/install.sh
+++ os/install.sh
@@ -1,3 +1,19 @@
 if [ "$(uname)" == "Darwin" ]; then
   defaults write NSGlobalDomain KeyRepeat -int 1
+
+  # Custom keyboard shortcuts
+  # @: CMD
+  # $: Shift
+  # ~: Alt
+  # ^: Ctrl
+  TAB_KEY_SYMBOL="\\U21e5" # Note the backslash-escaped backslash.
+  COMMAND_KEY_SYMBOL="@"
+  SHIFT_KEY_SYMBOL="$"
+  ENTER_KEY_SYMBOL="\\U21a9"
+  defaults write com.apple.finder NSUserKeyEquivalents "{ 'Show Package Contents' = '${COMMAND_KEY_SYMBOL}${SHIFT_KEY_SYMBOL}O'; }"
+
+  defaults write -g NSUserKeyEquivalents "{ 'Paste and Match Style' = '${COMMAND_KEY_SYMBOL}V'; }"
+  defaults write -g NSUserKeyEquivalents "{ 'Zoom' = '${COMMAND_KEY_SYMBOL}${SHIFT_KEY_SYMBOL}M'; }"
+
+  defaults write com.apple.Mail NSUserKeyEquivalents "{ 'Send' = '${COMMEND_KEY_SYMBOL}${ENTER_KEY_SYMBOL}'; }"
 fi
