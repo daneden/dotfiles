438ac1c8bb5e0741b660fa8eaf5fa55d413d3399
Daniel Eden <dan.eden@me.com>
2020-02-15 21:12:36 +0000

    Remove unused/cluttering configs & installers

--- lein.symlink/profiles.clj
+++
@@ -1,1 +0,0 @@
-{:user {:plugins [[lein-exec "0.3.6"]]}}
--- ruby/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install rvm
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable --ruby=2.3.0
-
-rvm --default use 2.3.0
