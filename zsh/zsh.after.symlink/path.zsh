92d1f7507d3526806cad7a49fa429295d8bea717
Daniel Eden <dan.eden@me.com>
2020-02-15 21:13:11 +0000

    Remove unused path configs

--- zsh/zsh.after.symlink/path.zsh
+++ zsh/zsh.after.symlink/path.zsh
@@ -4,10 +4,6 @@ elif [ -d "$HOME/.rvm" ]; then
   [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 fi
 
-if [ -d "$HOME/.linuxbrew" ]; then
-  export PATH="$HOME/.linuxbrew/bin:$PATH"
-fi
-
 if [ -d "$HOME/.npm-modules" ]; then
   export PATH="$HOME/.npm-modules/bin:$PATH"
 fi
@@ -15,7 +11,5 @@ fi
 if [ -d "$HOME/.yarn" ]; then
   export PATH="$HOME/.yarn/bin:$PATH"
 fi
-
-export PYTHONPATH="`which python3`:$PYTHONPATH"
 
 export PATH=/usr/local/bin:$PATH
