6d0bacffde5b82a8c2108b2e1675b987788adfe0
Daniel Eden <dan.eden@me.com>
2020-12-08 19:37:43 +0000

    Add git clone alias

--- zsh/zsh.after.symlink/alias.zsh
+++ zsh/zsh.after.symlink/alias.zsh
@@ -21,13 +21,21 @@ alias shorten='__shorten'
 # A utility function to clone a GitHub repo and cd into it
 function __gcd {
   if [ "$1x" != "x" ]; then
-    git clone "https://github.com/$1"
+    git clone "https://github.com/$1" --depth=10
     DIRNAME="$(cut -d'/' -f2 <<<$1)"
     cd "$DIRNAME"
   fi
 }
 
+# Clone a GitHub repo
+function __gc {
+  if [ "$1x" != "x" ]; then
+    git clone "https://github.com/$1" --depth=10
+  fi
+}
+
 alias gcd='__gcd'
+alias gc='__gc'
 
 # Some convenience aliases
 alias pull="git pull"
