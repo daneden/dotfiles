223b6b77b229c9557c84e26794e9a65e8edd777f
Daniel Eden <dan.eden@me.com>
2020-12-09 17:21:25 +0000

    Add plugin manager for zsh

---
+++ zsh/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Downloading Antigen (zsh plugin manager)..."
+curl -L git.io/antigen > antigen.zsh
---
+++ zsh/zsh.after.symlink/antigen.config.zsh
@@ -0,0 +1,12 @@
+source ${HOME}/.dotfiles/antigen.zsh
+
+# This initial apply is here to circumvent a caching bug
+antigen apply
+
+# Install plugins
+antigen bundle git
+antigen bundle command-not-found
+antigen bundle zsh-users/zsh-syntax-highlighting
+
+# Chocks away
+antigen apply
